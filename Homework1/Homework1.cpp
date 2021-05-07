727e672aaaf951478548635443b9f1946cdb68e7
Crazyayane07 <crazyayane07@gmail.com>
2021-05-07 15:02:43 +0200

    init

---
+++ Homework1/Homework1.cpp
@@ -0,0 +1,31 @@
+#include "Homework1.h"
+
+int main() {
+	std::cout << "Hello World!" << "\n";
+	return 0;
+}
+
+std::string calculate(const std::string& command, int first, int second)
+{
+	return "";
+}
+
+int fibonacci_iterative(int sequence)
+{
+	return 0;
+}
+
+int fibonacci_recursive(int sequence)
+{
+	return 0;
+}
+
+int NWW(int a, int b)
+{
+	return 0;
+}
+
+int NWD(int a, int b)
+{
+	return 0;
+}
---
+++ Homework1/Homework1.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+std::string calculate(const std::string& command, int first, int second);
+
+int fibonacci_iterative(int sequence);
+
+int fibonacci_recursive(int sequence);
+
+int NWW(int a, int b);
+
+int NWD(int a, int b);
