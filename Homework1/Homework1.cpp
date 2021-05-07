ea11acd830aa52b90c4faf8fd13670e4752e2e5a
Crazyayane07 <crazyayane07@gmail.com>
2021-05-07 15:23:50 +0200

    implemented calculate function

--- Homework1/Homework1.cpp
+++ Homework1/Homework1.cpp
@@ -1,13 +1,67 @@
 #include "Homework1.h"
+#include <string>
 
 int main() {
-	std::cout << "Hello World!" << "\n";
+
+	std::cout << "Zadanie 1:" << "\n";
+	std::cout << "\n";
+
+	std::cout << "2 + 6 = " << calculate("add", 2, 6) << "\n";
+	std::cout << "-2 + 6 = " << calculate("add", -2, 6) << "\n";
+	std::cout << "-2 + -6 = " << calculate("add", -2, -6) << "\n";
+	std::cout << "\n";
+
+	std::cout << "2 - 6 = " << calculate("substract", 2, 6) << "\n";
+	std::cout << "-2 - 6 = " << calculate("substract", -2, 6) << "\n";
+	std::cout << "-2 - -6 = " << calculate("substract", -2, -6) << "\n";
+	std::cout << "\n";
+
+	std::cout << "2 * 6 = " << calculate("multiply", 2, 6) << "\n";
+	std::cout << "-2 * 6 = " << calculate("multiply", -2, 6) << "\n";
+	std::cout << "-2 * -6 = " << calculate("multiply", -2, -6) << "\n";
+	std::cout << "\n";
+
+	std::cout << "2 / 6 = " << calculate("divide", 2, 6) << "\n";
+	std::cout << "-2 / 6 = " << calculate("divide", -2, 6) << "\n";
+	std::cout << "-2 / -6 = " << calculate("divide", -2, -6) << "\n";
+	std::cout << "\n";
+
+	std::cout << "-2 / -6 = " << calculate("bzdury", -2, -6) << "\n";
+	std::cout << "\n";
+	std::cout << "\n";
+
+	std::cout << "Zadanie 2:" << "\n";
+	std::cout << "\n";
+
 	return 0;
 }
 
 std::string calculate(const std::string& command, int first, int second)
 {
-	return "";
+	double result;
+
+	if (command == "add") 
+	{
+		result = first + (double)second;
+		return std::to_string(result);
+	}
+	else if (command == "substract")
+	{
+		result = first - (double)second;
+		return std::to_string(result);
+	}
+	else if (command == "multiply")
+	{
+		result = first * (double)second;
+		return std::to_string(result);
+	}
+	else if (command == "divide")
+	{
+		result = first / (double)second;
+		return std::to_string(result);
+	}
+
+	return "Invalid data";
 }
 
 int fibonacci_iterative(int sequence)
